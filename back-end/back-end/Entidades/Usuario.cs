160b5a5f299c2a9d19883ad41b4e312f7ab01841
Luis Alejandro Duarte Martinez <ladm4@hotmail.com>
2021-09-17 20:50:34 -0500

    Iniciar

---
+++ back-end/back-end/Entidades/Usuario.cs
@@ -0,0 +1,14 @@
+﻿
+namespace back_end.Entidades
+{
+    public class Usuario
+    {
+        public long Id { get; set; }
+
+        public string Login { get; set; }
+        public string Clave { get; set; }
+        public string Correo { get; set; }
+
+
+    }
+}
---
+++ back-end/back-end/Migrations/20210918004159_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace back_end.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Clave = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
